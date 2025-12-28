345747a5169ea289e962452b0d9aff06963a7cb2
Alfie Ardinata <88470966+relvinarsenio@users.noreply.github.com>
2025-12-28 06:02:39 +0000

    Add HTTP_CONNECT_TIMEOUT_SEC constant to configuration

--- include/config.hpp
+++ include/config.hpp
@@ -11,6 +11,7 @@ namespace Config {
     constexpr std::string_view SPEEDTEST_TGZ = "speedtest.tgz";
     
     constexpr long HTTP_TIMEOUT_SEC = 10;
+    constexpr long HTTP_CONNECT_TIMEOUT_SEC = 10;
     constexpr long SPEEDTEST_DL_TIMEOUT_SEC = 60;
     constexpr int UI_SPINNER_DELAY_MS = 150;
 }
