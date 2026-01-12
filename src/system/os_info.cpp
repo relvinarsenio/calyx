d49d47a6b587e4c90021ebbff60007a039ceb97f
Alfie Ardinata <88470966+relvinarsenio@users.noreply.github.com>
2026-01-12 15:54:21 +0700

    refactor: split monolithic system_info.cpp into cpu/os/storage modules

---
+++ src/system/os_info.cpp
@@ -0,0 +1,183 @@
+/*
+ * Copyright (c) 2025 Alfie Ardinata
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+#include "include/system_info.hpp"
+
+#include <array>
+#include <cstring>
+#include <filesystem>
+#include <fstream>
+#include <format>
+#include <string>
+
+#if defined(__i386__) || defined(__x86_64__)
+#include <cpuid.h>
+#endif
+
+#include <sys/sysinfo.h>
+#include <sys/utsname.h>
+#include <stdlib.h>
+
+namespace fs = std::filesystem;
+
+std::string SystemInfo::get_virtualization() {
+    if (fs::exists("/.dockerenv") || fs::exists("/run/.containerenv"))
+        return "Docker";
+
+    if (fs::exists("/proc/1/environ")) {
+        std::ifstream f("/proc/1/environ");
+        std::string env;
+        while (std::getline(f, env, '\0')) {
+            if (env.find("container=lxc") != std::string::npos)
+                return "LXC";
+        }
+    }
+
+    if (fs::exists("/proc/user_beancounters"))
+        return "OpenVZ";
+
+    struct utsname buffer;
+    if (uname(&buffer) == 0) {
+        std::string release = buffer.release;
+        if (release.find("Microsoft") != std::string::npos ||
+            release.find("WSL") != std::string::npos)
+            return "WSL";
+    }
+
+    if (fs::exists("/proc/1/environ")) {
+        std::ifstream f("/proc/1/environ");
+        std::string env;
+        while (std::getline(f, env, '\0')) {
+            if (env.find("WSL_DISTRO_NAME=") != std::string::npos ||
+                env.find("WSL_INTEROP=") != std::string::npos ||
+                env.find("WSLENV=") != std::string::npos) {
+                return "WSL";
+            }
+        }
+    }
+
+    if (fs::exists("/dev/dxg")) {
+        return "WSL";
+    }
+    if (fs::exists("/dev/lxss")) {
+        return "WSL";
+    }
+
+    if (fs::exists("/usr/lib/wsl") || fs::exists("/mnt/wsl")) {
+        return "WSL";
+    }
+
+    bool hv_bit = false;
+#if defined(__x86_64__) || defined(__i386__)
+    unsigned int eax, ebx, ecx, edx;
+    __cpuid(1, eax, ebx, ecx, edx);
+    if (ecx & (1U << 31))
+        hv_bit = true;
+#endif
+
+    if (hv_bit) {
+#if defined(__x86_64__) || defined(__i386__)
+        unsigned int leaf = 0x40000000;
+        __cpuid(leaf, eax, ebx, ecx, edx);
+
+        std::array<char, 13> vendor{};
+        std::memcpy(vendor.data(), &ebx, 4);
+        std::memcpy(vendor.data() + 4, &ecx, 4);
+        std::memcpy(vendor.data() + 8, &edx, 4);
+        vendor[12] = '\0';
+
+        std::string sig(vendor.data());
+
+        if (sig == "KVMKVMKVM")
+            return "KVM";
+        if (sig == "Microsoft Hv")
+            return "Hyper-V";
+        if (sig == "VMwareVMware")
+            return "VMware";
+        if (sig == "XenVMMXenVMM")
+            return "Xen";
+        if (sig == "VBoxVBoxVBox")
+            return "VirtualBox";
+        if (sig == "prl hyperv  ")
+            return "Parallels";
+        if (sig == "TCGTCGTCGTCG")
+            return "QEMU";
+#endif
+    }
+
+    std::ifstream dmi("/sys/class/dmi/id/product_name");
+    std::string product;
+    if (std::getline(dmi, product)) {
+        if (product.find("KVM") != std::string::npos)
+            return "KVM";
+        if (product.find("QEMU") != std::string::npos)
+            return "QEMU";
+        if (product.find("VirtualBox") != std::string::npos)
+            return "VirtualBox";
+    }
+
+    return hv_bit ? "Dedicated (Virtual)" : "Dedicated";
+}
+
+std::string SystemInfo::get_os() {
+    std::ifstream f("/etc/os-release");
+    std::string line;
+    while (std::getline(f, line)) {
+        if (line.starts_with("PRETTY_NAME=")) {
+            auto val = line.substr(12);
+            if (val.size() >= 2 && val.front() == '"')
+                val = val.substr(1, val.size() - 2);
+            return val;
+        }
+    }
+    return "Linux";
+}
+
+std::string SystemInfo::get_arch() {
+    struct utsname buffer;
+    if (uname(&buffer) == 0) {
+        std::string arch = buffer.machine;
+        int bits = static_cast<int>(sizeof(void*) * 8);
+        return std::format("{} ({} Bit)", arch, bits);
+    }
+    return "Unknown";
+}
+
+std::string SystemInfo::get_kernel() {
+    struct utsname buffer;
+    if (uname(&buffer) == 0)
+        return buffer.release;
+    return "Unknown";
+}
+
+std::string SystemInfo::get_tcp_cc() {
+    std::ifstream f("/proc/sys/net/ipv4/tcp_congestion_control");
+    std::string s;
+    if (f >> s)
+        return s;
+    return "Unknown";
+}
+
+std::string SystemInfo::get_uptime() {
+    struct sysinfo si;
+    if (sysinfo(&si) == 0) {
+        auto up = si.uptime;
+        int days = static_cast<int>(up / 86400);
+        int hours = static_cast<int>((up % 86400) / 3600);
+        int mins = static_cast<int>((up % 3600) / 60);
+        return std::format("{} days, {} hour {} min", days, hours, mins);
+    }
+    return "Unknown";
+}
+
+std::string SystemInfo::get_load_avg() {
+    double loads[3];
+    if (getloadavg(loads, 3) != -1) {
+        return std::format("{:.2f}, {:.2f}, {:.2f}", loads[0], loads[1], loads[2]);
+    }
+    return "Unknown";
+}
---
+++ src/system/storage_info.cpp
@@ -0,0 +1,231 @@
+/*
+ * Copyright (c) 2025 Alfie Ardinata
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+#include "include/system_info.hpp"
+#include "include/utils.hpp"
+
+#include <cctype>
+#include <charconv>
+#include <fstream>
+#include <format>
+#include <sstream>
+#include <string>
+#include <string_view>
+#include <vector>
+
+#include <sys/stat.h>
+#include <sys/statvfs.h>
+#include <sys/sysinfo.h>
+#include <sys/sysmacros.h>
+
+namespace {
+
+std::string capitalize(std::string_view s) {
+    if (s.empty())
+        return {};
+    std::string ret(s);
+    ret[0] = static_cast<char>(std::toupper(static_cast<unsigned char>(ret[0])));
+    if (ret == "Zram")
+        return "ZRAM";
+    return ret;
+}
+
+}  // namespace
+
+MemInfo SystemInfo::get_memory_status() {
+    MemInfo info{};
+    struct sysinfo si;
+
+    if (sysinfo(&si) == 0) {
+        info.total = si.totalram * si.mem_unit;
+    }
+
+    std::ifstream meminfo("/proc/meminfo");
+    std::string line;
+    uint64_t mem_available = 0;
+
+    while (std::getline(meminfo, line)) {
+        if (line.starts_with("MemAvailable:")) {
+            std::string_view sv = line;
+            auto colon = sv.find(':');
+            if (colon != std::string_view::npos) {
+                sv = sv.substr(colon + 1);
+                sv = trim_sv(sv);
+
+                auto [ptr, ec] = std::from_chars(sv.data(), sv.data() + sv.size(), mem_available);
+                if (ec == std::errc()) {
+                    mem_available *= 1024;
+                } else {
+                    mem_available = 0;
+                }
+                break;
+            }
+        }
+    }
+
+    if (mem_available > 0) {
+        info.available = mem_available;
+    } else {
+        info.available = si.freeram * si.mem_unit;
+    }
+
+    if (info.total >= info.available) {
+        info.used = info.total - info.available;
+    } else {
+        info.used = 0;
+    }
+
+    return info;
+}
+
+DiskInfo SystemInfo::get_disk_usage(const std::string& mountpoint) {
+    DiskInfo info{};
+    struct statvfs disk;
+
+    if (statvfs(mountpoint.c_str(), &disk) == 0) {
+        info.total = disk.f_blocks * disk.f_frsize;
+        info.free = disk.f_bfree * disk.f_frsize;
+        info.used = (disk.f_blocks - disk.f_bfree) * disk.f_frsize;
+    }
+
+    return info;
+}
+
+std::vector<SwapEntry> SystemInfo::get_swaps() {
+    std::vector<SwapEntry> swaps;
+
+    std::ifstream f("/proc/swaps");
+    std::string line;
+
+    if (std::getline(f, line)) {
+        while (std::getline(f, line)) {
+            std::stringstream ss(line);
+            std::string path, type, size_str, used_str;
+
+            if (ss >> path >> type >> size_str >> used_str) {
+                SwapEntry entry;
+                entry.path = path;
+
+                if (path.find("zram") != std::string::npos) {
+                    entry.type = "ZRAM";
+                } else {
+                    entry.type = capitalize(type);
+                }
+
+                uint64_t val_size = 0, val_used = 0;
+                auto [p1, ec1] =
+                    std::from_chars(size_str.data(), size_str.data() + size_str.size(), val_size);
+                auto [p2, ec2] =
+                    std::from_chars(used_str.data(), used_str.data() + used_str.size(), val_used);
+
+                if (ec1 == std::errc() && ec2 == std::errc()) {
+                    entry.size = val_size * 1024;
+                    entry.used = val_used * 1024;
+                    swaps.push_back(entry);
+                }
+            }
+        }
+    }
+
+    std::ifstream z("/sys/module/zswap/parameters/enabled");
+    char c;
+    if (z >> c && (c == 'Y' || c == 'y' || c == '1')) {
+        SwapEntry zswap;
+        zswap.type = "ZSwap";
+        zswap.path = "Enabled";
+        zswap.size = 0;
+        zswap.used = 0;
+        zswap.is_zswap = true;
+        swaps.push_back(zswap);
+    }
+
+    return swaps;
+}
+
+std::string SystemInfo::get_device_name(const std::string& path) {
+    struct stat st {};
+    if (stat(path.c_str(), &st) != 0)
+        return "unknown device";
+
+    std::ifstream mountinfo("/proc/self/mountinfo");
+    if (!mountinfo)
+        return "unknown device";
+
+    const std::string target_dev = std::format("{}:{}", major(st.st_dev), minor(st.st_dev));
+
+    std::string best_path_match = "unknown device";
+    size_t best_path_len = 0;
+
+    std::string exact_dev_match;
+
+    std::string line;
+    while (std::getline(mountinfo, line)) {
+        std::string_view line_view(line);
+        std::vector<std::string_view> tokens;
+
+        size_t start = 0;
+        while (start < line_view.length()) {
+            while (start < line_view.length() &&
+                   (line_view[start] == ' ' || line_view[start] == '\t'))
+                start++;
+            if (start >= line_view.length())
+                break;
+
+            size_t end = start;
+            while (end < line_view.length() && line_view[end] != ' ' && line_view[end] != '\t')
+                end++;
+
+            tokens.push_back(line_view.substr(start, end - start));
+            start = end;
+        }
+
+        if (tokens.size() < 5)
+            continue;
+
+        std::string id(tokens[0]), parent(tokens[1]), major_minor(tokens[2]), root(tokens[3]),
+            mount_point(tokens[4]);
+
+        size_t dash_pos = 5;
+        while (dash_pos < tokens.size() && tokens[dash_pos] != "-")
+            dash_pos++;
+
+        if (dash_pos + 2 >= tokens.size())
+            continue;
+
+        std::string fs_type(tokens[dash_pos + 1]), source(tokens[dash_pos + 2]);
+
+        auto format_output = [&](const std::string& src, const std::string& fs) {
+            if (src == fs)
+                return src;
+            return std::format("{} ({})", src, fs);
+        };
+
+        if (major_minor == target_dev) {
+            exact_dev_match = format_output(source, fs_type);
+        }
+
+        if (path.compare(0, mount_point.size(), mount_point) == 0) {
+            bool valid_boundary = path.size() == mount_point.size() || mount_point == "/" ||
+                                  path[mount_point.size()] == '/';
+
+            if (valid_boundary && mount_point.size() > best_path_len) {
+                best_path_len = mount_point.size();
+                best_path_match = format_output(source, fs_type);
+            }
+        }
+    }
+
+    if (best_path_len > 0 && best_path_match != "unknown device") {
+        return best_path_match;
+    }
+
+    if (!exact_dev_match.empty()) {
+        return exact_dev_match;
+    }
+
+    return best_path_match;
+}
